5c0f2d4477ccf02d964d2ed2fe277059fba821a8
Murat Çorlu <muratcorlu@gmail.com>
2012-02-28 18:38:05 +0200

    An example code added

---
+++ example.php
@@ -0,0 +1,21 @@
+<?php
+
+require('fsdb.php');
+
+$db = new fsdb('db');
+
+$person = array('name'=> 'Murat', 'children' => array('Hasan', 'Zeki', 'Zehra'));
+
+$id = $db->save('people', $person);
+
+$person = $db->get('people', $id);
+$person['name'] = 'Hasan';
+$person['children'][0] = 'Murat';
+$db->save('people', $person);
+
+$people = $db->select('people');
+
+foreach($people as $id) {
+    $p = $db->get('people', $id);
+    print($p['id'] . ': ' . $p['name'] . "\n");
+}
