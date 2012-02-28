288f06706364cf87c319cfb85515c8c553581acd
Hasan Sama <hasan.sama@sahibinden.com>
2012-02-28 18:16:52 +0200

    basic implementations

--- fsdb.php
+++ fsdb.php
@@ -5,35 +5,49 @@ class fsdb {
 	private $db_path;
 	
 	function __construct($path='db') {
-		$this->db_path = __DIR__ . DIRECTORY_SEPERATOR . trim($path, DIRECTORY_SEPERATOR);
+		$this->db_path = __DIR__ . DIRECTORY_SEPARATOR . trim($path, DIRECTORY_SEPARATOR);
+		if (! is_dir($this->db_path) ) {
+	        mkdir( $this->db_path );
+	    }
 	}
 	
 	/**
 	 * get data from a collection by given id
 	 */
 	function get($collection, $id) {
-	    $file = $this->db_path . DIRECTORY_SEPERATOR . $collection . DIRECTORY_SEPERATOR . "$id.txt";
+	    $file = $this->db_path . DIRECTORY_SEPARATOR . $collection . DIRECTORY_SEPARATOR . "$id.txt";
 	    if (! is_file($file) ) {
 	        return false;
 	    }
 	    return unserialize(file_get_contents($file));
 	}
 		
-	function list($collection, $offset=0, $size=10) {
-	    
+	function select($collection, $offset=0, $size=10) {
+	    $file_list = explode("\n", shell_exec('ls -l -C1 ' . $this->db_path . DIRECTORY_SEPARATOR . $collection.DIRECTORY_SEPARATOR . '*.txt'));
+		$result =  array_slice($file_list, $offset, $size);
+
+		$response = array();
+		foreach($result as $row){
+			if (!empty($row)){
+				array_push($response, basename($row, '.txt'));
+			}
+		}
+			
+		return $response;
 	} 
 	
 	function save($collection, $data) {
-	    $id = is_exist($data['id']) ? $data['id'] : $this->get_next_id( $collection );
-	    $data['id'] = $id;
-	    
-	    $folder = $this->db_path . DIRECTORY_SEPERATOR . $collection;
+		$folder = $this->db_path . DIRECTORY_SEPARATOR . $collection;
 	    if (! is_dir($folder) ) {
 	        mkdir( $folder );
 	    }
-	    
-	    
-	    $file = $folder . DIRECTORY_SEPERATOR . "$id.txt";
+
+		if (!isset($data['id'])){
+			$data['id'] = $this->generate_id($collection);
+		}
+	    $id = $data['id'];
+	
+	    $file = $folder . DIRECTORY_SEPARATOR . "$id.txt";
 	    
 	    return file_put_contents($file, serialize($data));
 	    
@@ -41,6 +55,17 @@ class fsdb {
 	
 	function delete($collection, $id) {
 	    
+    }
+
+	private function generate_id( $collection ){
+		$file = $this->db_path . DIRECTORY_SEPARATOR . $collection . DIRECTORY_SEPARATOR;
+		$current_time =	date('Ymdhis');
+		$iteration = 0;
+		while(is_file($file . $current_time . $iteration . '.txt')){
+			$iteration++;
+		}
+		
+		return $current_time . $iteration;
 	}
 	
 }
